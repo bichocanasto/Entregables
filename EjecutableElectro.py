ae0f1e9ed460902d9beec0f5345e916094b92402
Carolina Maffei <160056215+bichocanasto@users.noreply.github.com>
2024-10-02 12:56:16 -0300

    Update EjecutableElectro.py
    Completo y ordenado

--- EjecutableElectro.py
+++ EjecutableElectro.py
@@ -14,17 +14,14 @@ class EjecutableElectro(Electrodomestico):
         Lavadora.__init__(self,carga)
         Televisor.__init__(self, resolucion, sintonizador)
 
-    def precioFinal(self):
-        for elements in arrayelectro:
-            return super().precioFinal()
-
 
 electro1 = Electrodomestico(200,"NEGRO","D",45)
 electro2 = Electrodomestico(500,"ROJO","B",10)
 electro3 = Electrodomestico(300,"VERDE","C",20)
+electro4 = Electrodomestico(150,"MAGENTA","F",100)
 lava01 = Lavadora(10,500,"AZUL","B",65)
 lava02 = Lavadora(6,250,"BLANCO", "D",90)
-lava02 = Lavadora(15,1000,"NEGRA","A",55)
+lava03 = Lavadora(15,1000,"NEGRA","A",55)
 tele01 = Televisor(25,True,250,"AZUL","C",70)
 tele02 = Televisor(40,False,400,"VERDE","B",80)
 tele03 = Televisor(50,True,800,"BLANCO","A",55)
@@ -34,15 +31,36 @@ arrayelectro = []
 arrayelectro.append(electro1)
 arrayelectro.append(electro2)
 arrayelectro.append(electro3)
+arrayelectro.append(electro4)
 arrayelectro.append(lava01)
 arrayelectro.append(lava02)
+arrayelectro.append(lava03)
 arrayelectro.append(tele01)
 arrayelectro.append(tele02)
 arrayelectro.append(tele03)
 
 
+precioTele = 0
+precioLava = 0
+precioElectro = 0
 
-print(arrayelectro[0].precioFinal())
+for i in range (len(arrayelectro)):
+    print(arrayelectro[i].precioFinal())
+    print(arrayelectro[i])
+    precioElectro += arrayelectro[i].get_precioBase()
+    if hasattr(arrayelectro[i], "carga") == True: 
+        precioLava += arrayelectro[i].get_precioBase()
+    if hasattr(arrayelectro[i], "resolucion") == True: 
+        precioTele += arrayelectro[i].get_precioBase()
+
+
+#3.8.4: Deberás mostrar el precio de cada clase, es decir, el precio de todas las
+#televisiones por un lado, el de las lavadoras por otro y la suma de los Electrodomesticos (puedes crear objetos Electrodomestico, pero recuerda que Television y Lavadora también son electrodomésticos).
+#Por ejemplo, si tenemos un Electrodomestico con un precio final de 300, una lavadora de 200 y una televisión de 500, el resultado final sera de 1000 (300+200+500) para electrodomésticos, 200 para lavadora y 500 para televisión.
+
+print("Precio de los electrodomesticos: " + str(precioElectro))
+print("Precio de los televisores: " + str(precioTele))
+print("Precio de las lavadoras: " + str(precioLava))
 
 
 
